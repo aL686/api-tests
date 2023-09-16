fb60183b874a104699a5675bdc102623a612288c
admin <admin@admin.ru>
2023-09-16 18:11:08 +0300

    /

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,25 +1,49 @@
-currentBuild.displayName = "main"
-base_git_url = "https://github.com/aL686/ui-tests.git"
+task_branch = "${TEST_BRANCH_NAME}"
+def branch_cutted = task_branch.contains("origin") ? task_branch.split('/')[1] : task_branch.trim()
+currentBuild.displayName = "$branch_cutted"
+base_git_url = "https://gitlab.com/epickonfetka/cicd-threadqa.git"
 
 
 node {
-    stage("Checkout Branch") {
-        getProject("$base_git_url", "main")
-    }
-    try {
-        stage("Run Tests") {
-            parallel runTestWithTag()
+    withEnv(["branch=${branch_cutted}", "base_url=${base_git_url}"]) {
+        stage("Checkout Branch") {
+            if (!"$branch_cutted".contains("main")) {
+                try {
+                    getProject("$base_git_url", "$branch_cutted")
+                } catch (err) {
+                    echo "Failed get branch $branch_cutted"
+                    throw ("${err}")
+                }
+            } else {
+                echo "Current branch is main"
+            }
         }
-    } finally {
-        stage("Generate Allure Report") {
-            generateAllure()
+
+        try {
+            parallel getTestStages(["apiTests", "uiTests"])
+        } finally {
+            stage ("Allure") {
+                generateAllure()
+            }
         }
     }
 }
 
-def runTestWithTag() {
+
+def getTestStages(testTags) {
+    def stages = [:]
+    testTags.each { tag ->
+        stages["${tag}"] = {
+            runTestWithTag(tag)
+        }
+    }
+    return stages
+}
+
+
+def runTestWithTag(String tag) {
     try {
-        labelledShell(label: "Run main", script: "./gradlew -x test")
+        labelledShell(label: "Run ${tag}", script: "chmod +x gradlew \n./gradlew -x test ${tag}")
     } finally {
         echo "some failed tests"
     }
@@ -44,3 +68,5 @@ def generateAllure() {
             results          : [[path: 'build/allure-results']]
     ])
 }
+
+
